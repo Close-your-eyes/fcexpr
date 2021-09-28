d36fd1568b6798acea1504831061ca17a5532b85
Close-your-eyes <vonskopnik@pm.me>
2021-09-28 10:50:39 +0200

    data added

--- R/ws_get_popstats.R
+++ R/ws_get_popstats.R
@@ -35,41 +35,65 @@ ws_get_popstats <- function(ws, gr, FCS.file.folder, groupwise = T) {
         stop(paste0(FCS.file.folder, " not found."))
     }
 
-    packageVersion("CytoML")
+    boic.subversion <- as.numeric(strsplit(as.character(BiocManager::version()), "\\.")[[1]][2])
 
-    popStats.raw <- do.call(rbind, lapply(seq_along(ws), function(x) {
-        wsp <- CytoML::open_flowjo_xml(ws[x], sample_names_from = "sampleNode")
-        gr.wsp <- base::intersect(unique(CytoML::fj_ws_get_sample_groups(wsp)[, "groupName"]), gr[[x]])
-        ps <- if (!groupwise) {
-            do.call(rbind, lapply(gr.wsp, function(y) {
-                group_id <- unique(CytoML::fj_ws_get_sample_groups(wsp)[which(CytoML::fj_ws_get_sample_groups(wsp)$groupName == y), "groupID"]) +
-                  1
-                fcs.files <- CytoML::fj_ws_get_samples(wsp, group_id = group_id)[, "name"]
-                ps <- data.frame()
-                for (i in 1:length(fcs.files)) {
-                  ps <- as.data.frame(rbind(ps, flowWorkspace::gs_pop_get_count_fast(CytoML::flowjo_to_gatingset(ws = wsp, name = y, subset = i,
-                    path = FCS.file.folder, execute = F, emptyValue = F), path = "full", xml = T)))
-                }
-                ps[, "Group"] <- y
-                return(ps)
-            }))
-        } else {
-            do.call(rbind, lapply(gr.wsp, function(y) {
-                gs <- CytoML::flowjo_to_gatingset(wsp, name = y, path = FCS.file.folder, execute = F, emptyValue = F)
-                ps <- as.data.frame(flowWorkspace::gs_pop_get_count_fast(gs, path = "full", xml = T))
-                ps[, "Population"] <- flowWorkspace::gs_get_pop_paths(gs, path = "auto")[-1]
-                ps[, "Group"] <- y
-                return(ps)
-            }))
-        }
-        names(ps)[which(names(ps) == "name")] <- "FileName"
-        ps[, "PopulationFullPath"] <- gsub("^root", "", paste(ps[, "Parent"], sapply(sapply(base::strsplit(ps[, "Population"], "/"), rev), "[",
-            1), sep = "/"), "^root")
-        ps[, "FractionOfParent"] <- ps[, "Count"]/ps[, "ParentCount"] * 100
-        ps[, "wsp"] <- basename(ws[x])
-        ps <- ps[, c(1, 7, 2, 3, 4, 5, 8, 9, 6)]
-        return(ps)
-    }))
+    if (!groupwise && boic.subversion < 12) {
+        stop("Non-groupwise import not possible with the old Bioconductor version.")
+    }
+
+    popStats.raw <- if (boic.subversion < 12) {
+#### fix
+        wsp <- CytoML::open_flowjo_xml(wsps[x])
+        sample.groups <- base::intersect(unique(CytoML::fj_ws_get_sample_groups(wsp)[,"groupName"]), groups[[x]])
+
+        do.call(rbind, lapply(sample.groups, function(y) {
+            gs <- CytoML::flowjo_to_gatingset(wsp, name = y, path = FCS.file.folder, execute = T, emptyValue = F, which.lines = 1)
+            flowWorkspace::sampleNames(gs) <- sapply(1:length(gs), function(z) {basename(flowWorkspace::keyword(flowWorkspace::gh_pop_get_data(gs[[z]]))[["FILENAME"]])})
+            ps <- as.data.frame(flowWorkspace::gs_pop_get_count_fast(gs, path = "full", xml = T))
+            ps[,"Population"] <- flowWorkspace::gs_get_pop_paths(gs, path = "auto")[-1]
+            ps[,"Group"] <- y
+            return(ps)
+        }))
+
+    } else {
+
+        do.call(rbind, lapply(seq_along(ws), function(x) {
+            wsp <- CytoML::open_flowjo_xml(ws[x], sample_names_from = "sampleNode")
+            gr.wsp <- base::intersect(unique(CytoML::fj_ws_get_sample_groups(wsp)[, "groupName"]), gr[[x]])
+            ps <- if (!groupwise) {
+                do.call(rbind, lapply(gr.wsp, function(y) {
+                    group_id <- unique(CytoML::fj_ws_get_sample_groups(wsp)[which(CytoML::fj_ws_get_sample_groups(wsp)$groupName == y), "groupID"]) +
+                        1
+                    fcs.files <- CytoML::fj_ws_get_samples(wsp, group_id = group_id)[, "name"]
+                    ps <- data.frame()
+                    for (i in 1:length(fcs.files)) {
+                        ps <- as.data.frame(rbind(ps, flowWorkspace::gs_pop_get_count_fast(CytoML::flowjo_to_gatingset(ws = wsp, name = y, subset = i,
+                                                                                                                       path = FCS.file.folder, execute = F, emptyValue = F), path = "full", xml = T)))
+                    }
+                    ps[, "Group"] <- y
+                    return(ps)
+                }))
+            } else {
+                do.call(rbind, lapply(gr.wsp, function(y) {
+                    gs <- CytoML::flowjo_to_gatingset(wsp, name = y, path = FCS.file.folder, execute = F, emptyValue = F)
+                    ps <- as.data.frame(flowWorkspace::gs_pop_get_count_fast(gs, path = "full", xml = T))
+                    ps[, "Population"] <- flowWorkspace::gs_get_pop_paths(gs, path = "auto")[-1]
+                    ps[, "Group"] <- y
+                    return(ps)
+                }))
+            }
+            names(ps)[which(names(ps) == "name")] <- "FileName"
+            ps[, "PopulationFullPath"] <- gsub("^root", "", paste(ps[, "Parent"], sapply(sapply(base::strsplit(ps[, "Population"], "/"), rev), "[",
+                                                                                         1), sep = "/"), "^root")
+            ps[, "FractionOfParent"] <- ps[, "Count"]/ps[, "ParentCount"] * 100
+            ps[, "wsp"] <- basename(ws[x])
+            ps <- ps[, c(1, 7, 2, 3, 4, 5, 8, 9, 6)]
+            return(ps)
+        }))
+
+    }
+
+
 
     return(popStats.raw)
 }
