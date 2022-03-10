eaaf5509eb2fbbba55de4acc62f3398844db8c9a
Christopher Skopnik <christopher.skopnik@cohenfano.local>
2022-03-10 10:34:51 +0100

    up

--- R/gs_get_gates.R
+++ R/gs_get_gates.R
@@ -1,13 +1,12 @@
 #' Gates from gatingset for plotting with ggcyto
 #'
 #' @param gs gatingset
-#' @param bins number of bins for plotting
-#' @param low.lim lower range limit
-#' @param up.lim upper range limit
-#' @param up.lim.scatter upper range limit for scatter channels
-#' @param gate.pct.stat.x.pos x-position of percent labels for gates
-#' @param gate.pct.stat.y.pos y-position of percent labels for gates
-#' @param gate.pct.stat.size size of percent labels for gates
+#' @param quantile_lim_filter quantiles of signals to set axis limits to
+#' @param min_max_vals minimum and/or maximum required signal of one event in order to condider it for axis limit calculation (to filter extreme values)
+#' @param scatter_lim manual limits for scatter channel, set to NULL to get the actual limits (min and max)
+#' @param x_statpos x-position of percent labels for gates
+#' @param y_statpos y-position of percent labels for gates
+#' @param stat_size size of percent labels for gates
 #'
 #' @return a data frame to loop over and produce plots with ggcyto
 #' @export
@@ -27,13 +26,13 @@
 #'  p <- ggcyto(gs, subset = gg[1,"subset"], filter = my.filter, aes(!!sym(gg[1,"x"]), !!sym(gg[1,"y"])), max_nrow_to_plot = 5e4) +
 #'    geom_hex(bins = gg[1,"bins"]) +
 #'    theme_bw() +
-#'    xlab(gg[1,"x.lab"]) +
-#'    ylab(gg[1,"y.lab"]) +
-#'    ggcyto_par_set(limits = list(x = c(gg[1,"x.low.lim"], gg[1,"x.up.lim"]), y = c(gg[1,"y.low.lim"], gg[1,"y.up.lim"]))) +
+#'    xlab(gg[1,"x_lab"]) +
+#'    ylab(gg[1,"y_lab"]) +
+#'    ggcyto_par_set(limits = list(x = c(gg[1,"x_lowlim"], gg[1,"x.up.lim"]), y = c(gg[1,"y_lowlim"], gg[1,"y.up.lim"]))) +
 #'    scale_fill_gradientn(colours = scexpr::col_pal("spectral"), trans = "pseudo_log") +
 #'    theme(legend.position = "none", strip.background = element_rect(fill = "white"), text = element_text(family = "Courier"), panel.grid = element_blank(), axis.text = element_blank()) +
 #'    facet_grid(cols = vars(Patient, TCR), rows = vars(stimulus)) # order inline: facet_grid(cols = vars(PBMC.donor.short, factor(IL15.pre.stim.conc.ug.ml, levels=c("0", "0.12", "0.37", "1.11", "3.33", "10"))), rows = vars(RPTECs, RPTEC.IFNg.pre.stim))
-  ## loop through multiple lines of gg for gates
+## loop through multiple lines of gg for gates
 #'  for (i in 1:nrow(gg)) {
 #'    p <-
 #'      p +
@@ -45,22 +44,37 @@
 #'return(p)
 #'}))
 #'
-#'
 #' }
 gs_get_gates <- function(gs,
-                         bins = 200,
-                         low.lim = 0,
-                         up.lim = 300,
-                         up.lim.scatter = 250000,
-                         gate.pct.stat.x.pos = 0.8,
-                         gate.pct.stat.y.pos = 0.2,
-                         gate.pct.stat.size = 4) {
+                         binwidths = c(4,4),
+                         quantile_lim_filter = c(0.001, 0.999),
+                         min_max_vals = c(0, 300),
+                         scatter_lim = c(0, 250000),
+                         x_statpos = 0.8,
+                         y_statpos = 0.2,
+                         stat_size = 4) {
 
   if (!requireNamespace("flowWorkspace", quietly = T)){
     utils::install.packages("flowWorkspace")
   }
   if (!requireNamespace("flowCore", quietly = T)){
     BiocManager::install("flowCore")
+  }
+
+  if (!is.null(scatter_lim)) {
+    if (!is.numeric(scatter_lim) || length(scatter_lim) != 2) {
+      stop("scatter_lim has to be a numeric vector of length 2.")
+    }
+  }
+
+  if (!is.null(min_max_vals)) {
+    if (!is.numeric(min_max_vals) || length(min_max_vals) != 2) {
+      stop("min_max_vals has to be a numeric vector of length 2.")
+    }
+  }
+
+  if (is.null(quantile_lim_filter)) {
+    quantile_lim_filter <- c(0,1)
   }
 
   gates <-
@@ -70,23 +84,73 @@ gs_get_gates <- function(gs,
     dplyr::filter(gate.level > 0) %>%
     dplyr::mutate(subset = gsub("^/", "", dirname(gate.path.full))) %>%
     dplyr::mutate(subset = ifelse(subset == "", "root", subset)) %>%
-    dplyr::mutate(bins = bins, gate.pct.stat.x.pos = gate.pct.stat.x.pos, gate.pct.stat.y.pos = gate.pct.stat.y.pos, gate.pct.stat.size = gate.pct.stat.size)
+    dplyr::mutate(binwidths = list(binwidths), x_statpos = x_statpos, y_statpos = y_statpos, stat_size = stat_size)
 
-  gates$dims <- sapply(gates$gate.path.full, function(x) {list(unname(flowCore::parameters(flowWorkspace::gs_pop_get_gate(gs[[1]], x)[[1]])))})
+  gates$dims <- sapply(gates$gate.path.full, function(x) {
+    y <- unname(flowCore::parameters({flowWorkspace::gs_pop_get_gate(gs[[1]], x)[[1]]}))
+    return(y)
+    # stupid handling of Not-gate. other booleans may require similar specific treatment
+'    if (length(y) == 0) {
+      y <- list(unname(flowCore::parameters({flowWorkspace::gs_pop_get_gate(gs[[1]], gsub("^!", "", zz@deparse))[[1]]})))
+    } else {
+      return(list(y))
+    }'
+  })
+  ## filter boolean gates - test further ... # boolean are not easy to handle (e.g. their children)
+  gates <- gates[which(lengths(gates$dims) > 0),]
+
   gates$x <- unname(sapply(gates$dims, function(x) {unlist(x)[1]}))
   gates$y <- unname(sapply(gates$dims, function(x) {unlist(x)[2]}))
-  gates$x.lab <- unname(sapply(gates$dims, function(x) {unlist(x)[1]}))
-  gates$y.lab <- unname(sapply(gates$dims, function(x) {unlist(x)[2]}))
+  gates$x_lab <- unname(sapply(gates$dims, function(x) {unlist(x)[1]}))
+  gates$y_lab <- unname(sapply(gates$dims, function(x) {unlist(x)[2]}))
 
-  gates <-
-    gates %>%
-    dplyr::mutate(marginalFilter = ifelse(grepl("fsc|ssc", x, ignore.case = T) & grepl("fsc|ssc", y, ignore.case = T), T, F)) %>%
-    dplyr::mutate(x.low.lim = low.lim, x.up.lim = ifelse(grepl("fsc|ssc", x, ignore.case = T), up.lim.scatter, up.lim), y.low.lim = low.lim, y.up.lim = ifelse(grepl("fsc|ssc", y, ignore.case = T), up.lim.scatter, up.lim))
+
+
+  gates$marginalFilter <- ifelse(grepl("fsc|ssc", gates$x, ignore.case = T) & grepl("fsc|ssc", gates$y, ignore.case = T), T, F)
+
+  lims <- lapply(split(gates, 1:nrow(gates)), function(y) {
+    rel_cols <- do.call(rbind, lapply(seq_along(gs), function(x) {
+      parent <- dirname(y$gate.path.full)
+      if (parent == "/") {
+        parent <- "root"
+      }
+      expr <- flowCore::exprs(flowWorkspace::cytoframe_to_flowFrame(flowWorkspace::gh_pop_get_data(gs[[x]], y = parent)))[,c(y$x, y$y)]
+      if (!is.null(min_max_vals)) {
+        min_max_vals <- sort(min_max_vals)
+        colinds <- which(!grepl("fsc|ssc", colnames(expr), ignore.case = T))
+        if (length(colinds) > 0) {
+          expr <- expr[which(apply(expr[,colinds], 1, function(x) all(x>min_max_vals[1]))),]
+          expr <- expr[which(apply(expr[,colinds], 1, function(x) all(x<min_max_vals[2]))),]
+        }
+      }
+      return(expr)
+    }))
+    ret <- apply(rel_cols, 2, quantile, quantile_lim_filter)
+    ret <- c(ret[,1], ret[,2])
+    return(ret)
+  })
+
+  # order is known (see ret)
+  gates$x_lowlim <- sapply(lims, "[", 1)
+  gates$x_uplim <- sapply(lims, "[", 2)
+  gates$y_lowlim <- sapply(lims, "[", 3)
+  gates$y_uplim <- sapply(lims, "[", 4)
+
+  if (!is.null(scatter_lim)) {
+    scatter_lim <- sort(scatter_lim)
+    gates[which(grepl("fsc|ssc", gates$x, ignore.case = T)),"x_lowlim"] <- scatter_lim[1]
+    gates[which(grepl("fsc|ssc", gates$x, ignore.case = T)),"x_uplim"] <- scatter_lim[2]
+    gates[which(grepl("fsc|ssc", gates$y, ignore.case = T)),"y_lowlim"] <- scatter_lim[1]
+    gates[which(grepl("fsc|ssc", gates$y, ignore.case = T)),"y_uplim"] <- scatter_lim[2]
+  }
 
   gates$facet.strip <- c(T, rep(F, nrow(gates)-1))
 
   return(gates)
 }
+
+
+
 
 
 
--- inst/extdata/20220206_cluster_bench.R
+++ inst/extdata/20220206_cluster_bench.R
@@ -12,8 +12,8 @@ wsp <- wsps[1]
 
 
 
-ffs <- fcexpr::wsp_get_ff(wsp, groups = "Pack 8", population = "PCsub_IgA_TACI", FCS.file.folder = file.path(dirname(drfz), "FCS_files"),
-                          lapply_fun = parallel::mclapply, mc.cores = 3)
+ffs <- fcexpr::wsp_get_ff(wsp, groups = c("Pack 1", "Pack 2", "Pack 3", "Pack 4", "Pack 5", "Pack 6", "Pack 7", "Pack 8", "Pack 9"), population = "PCsub_IgA_TACI", FCS.file.folder = file.path(dirname(drfz), "FCS_files"),
+                          lapply_fun = parallel::mclapply, mc.cores = 6)
 ffs <- ffs[[1]]
 
 
@@ -29,8 +29,15 @@ nrow(expr)
 
 
 #### round 1
+
+# cluster_walktrap failed at 50k:
+# Error in graph.adjacency.dense(adjmatrix, mode = mode, weighted = weighted,  : long vectors not supported yet: /Volumes/Builds/R4/R-4.1.0/src/include/Rinlinedfuns.h:537
+# cluster louvain failed at 50k cells (memory exhausted)
+# hclust failed at 100k cells
+
+
 clust_bench <- bench::press(
-  n = c(1e3, 5e3, 1e4), #n = c(1e3, 5e3, 1e4, 5e4, 1e5, 2.5e5, 5e5, 1e6),
+  n = c(1e3, 5e3, 1e4, 5e4, 1e5, 5e5, 1e6, 1e7), #n = c(1e3, 5e3, 1e4, 5e4, 1e5, 2.5e5, 5e5, 1e6),
 
   {
     X <- expr[sample(1:nrow(expr), n, replace = n > nrow(expr)),]
@@ -46,6 +53,9 @@ clust_bench <- bench::press(
       },
       "ClusterR::KMeans_arma" = {
         ClusterR::KMeans_arma(X, clusters = 10)
+      },
+      "ClusterR::KMeans_rcpp" = {
+        ClusterR::KMeans_rcpp(X, clusters = 10)
       },
       "ClusterR::MiniBatchKmeans" = {
         predict_MBatchKMeans(X, MiniBatchKmeans(X, clusters = 10, batch_size = 20, num_init = 5, max_iters = 100,
@@ -53,36 +63,62 @@ clust_bench <- bench::press(
                                                    verbose = F)$centroids)
       },
       "louvain_seurat" = {
-        Seurat::FindClusters(Seurat::FindNeighbors(X, annoy.metric = "cosine")$snn)
+        if (nrow(X) <= 5e5) {
+          Seurat::FindClusters(Seurat::FindNeighbors(X, annoy.metric = "cosine")$snn)
+        } else {
+          NULL
+        }
       },
       "leiden" = {
-        leiden::leiden(Seurat::FindNeighbors(X, annoy.metric = "cosine")$snn)
+        if (nrow(X) <= 1e4) {
+          leiden::leiden(Seurat::FindNeighbors(X, annoy.metric = "cosine")$snn)
+        } else {
+          NULL
+        }
       },
       "hclust" = {
-        stats::cutree(stats::hclust(stats::dist(X)), k = 10)
+        if (nrow(X) <= 5e4) {
+          stats::cutree(stats::hclust(stats::dist(X)), k = 10)
+        } else {
+          NULL
+        }
       },
       "flowClust" = {
-        flowClust::flowClust(X, K = 10)
+        if (nrow(X) <= 1e5) {
+          flowClust::flowClust(X, K = 10)
+        } else {
+          NULL
+        }
       },
       "MUDAN_igraph::cluster_walktrap" = {
-        message("cluster_walktrap")
-        MUDAN::getComMembership(X, k = 30, method = igraph::cluster_walktrap)
+        if (nrow(X) <= 1e4) {
+          MUDAN::getComMembership(X, k = 30, method = igraph::cluster_walktrap)
+        } else {
+          NULL
+        }
       },
       "MUDAN_igraph::cluster_louvain" = {
-        message("cluster_louvain")
-        MUDAN::getComMembership(X, k = 30, method = igraph::cluster_louvain)
+        if (nrow(X) <= 1e4) {
+          MUDAN::getComMembership(X, k = 30, method = igraph::cluster_louvain)
+        } else {
+          NULL
+        }
       },
       "MUDAN_igraph::cluster_infomap" = {
-        message("cluster_infomap")
-        MUDAN::getComMembership(X, k = 30, method = igraph::cluster_infomap)
+        if (nrow(X) <= 1e4) {
+          MUDAN::getComMembership(X, k = 30, method = igraph::cluster_infomap)
+        } else {
+          NULL
+        }
       }
     )
   }
 )
 
-
 clust_bench$method = attr(clust_bench$expression, "description")
 d <- as_tibble(clust_bench) %>% select(-expression, -result, -memory, -time, -gc)
+d <- d[which(d$mem_alloc != "0B"),]
+saveRDS(d, file.path(wd, "20220309_bench_result.RDS"))
 
 # https://github.com/slowkow/slowkow.com/blob/master/content/notes/pca-benchmark/index.Rmd
 p1 <- ggplot(d, aes(n, median, color = method, label = reorder(method, n / median))) +
@@ -90,7 +126,7 @@ p1 <- ggplot(d, aes(n, median, color = method, label = reorder(method, n / media
   guides(color = guide_legend(reverse = FALSE,
                               override.aes = list(label = levels(fct_reorder(d$method, d$n / d$median)), size = 5),
                               label = FALSE)) +
-  ggrepel::geom_text_repel(data = d %>% filter(n == max(n)),
+  ggrepel::geom_text_repel(data = d %>% dplyr::group_by(method) %>% filter(n == max(n)),
                            mapping = aes(label = glue::glue("{signif(median, 3)}s")),
                            size = 6,
                            nudge_x = 0.1,
@@ -106,7 +142,7 @@ p1 <- ggplot(d, aes(n, median, color = method, label = reorder(method, n / media
 p2 <- ggplot(d, aes(n, mem_alloc, color = method, label = reorder(method, -n / median))) +
   geomtextpath::geom_textline(size = 5.5, text_smoothing = 30) +
   guides(color = guide_legend(reverse = TRUE, override.aes = list(label = "", size = 3))) +
-  ggrepel::geom_text_repel(data = d %>% filter(n == max(n)),
+  ggrepel::geom_text_repel(data = d %>% dplyr::group_by(method) %>% filter(n == max(n)),
                            mapping = aes(label = as.character(mem_alloc)),
                            size = 6,
                            nudge_x = 0.1,
@@ -120,106 +156,7 @@ p2 <- ggplot(d, aes(n, mem_alloc, color = method, label = reorder(method, -n / m
   labs(title = "GB allocated", x = "Cells")
 
 p_grid <- cowplot::plot_grid(p1, p2)
-ggsave(p_grid, filename = paste0("first_round_1k_to_10k_cells.png"), device = "png", path = wd, dpi = "retina", width = 18, height = 8)
+ggsave(p_grid, filename = paste0("1k_to_10M_cells.png"), device = "png", path = wd, dpi = "retina", width = 18, height = 8)
 
 
-
-
-
-
-
-
-
-
-ffs <- fcexpr::wsp_get_ff(wsp, groups = c("Pack 1", "Pack 2", "Pack 3", "Pack 4", "Pack 5", "Pack 6", "Pack 7", "Pack 8", "Pack 9"), population = "PCsub_IgA_TACI", FCS.file.folder = file.path(dirname(drfz), "FCS_files"),
-                          lapply_fun = parallel::mclapply, mc.cores = 6)
-ffs <- ffs[[1]]
-
-
-channels <- setNames(ffs[["logicle"]][[1]]@parameters@data[["name"]], ffs[["logicle"]][[1]]@parameters@data[["desc"]])
-channels <- channels[which(!is.na(names(channels)))]
-expr <- do.call(rbind, lapply(ffs[["logicle"]], function(x) flowCore::exprs(x)))[,channels]
-nrow(expr)
-
-#### round 2
-clust_bench <- bench::press(
-  n = c(1e4, 5e4, 1e5),
-
-  {
-    X <- expr[sample(1:nrow(expr), n, replace = n > nrow(expr)),]
-    rownames(X) <- 1:nrow(X)
-    #browser()
-    bench::mark(
-      check = FALSE,
-      time_unit = 's',
-      max_iterations = 3,
-
-      "stats::kmeans" = {
-        stats::kmeans(X, centers = 10)
-      },
-      "ClusterR::KMeans_arma" = {
-        ClusterR::KMeans_arma(X, clusters = 10)
-      },
-      "ClusterR::MiniBatchKmeans" = {
-        predict_MBatchKMeans(X, MiniBatchKmeans(X, clusters = 10, batch_size = 20, num_init = 5, max_iters = 100,
-                                                init_fraction = 0.2, initializer = 'kmeans++', early_stop_iter = 10,
-                                                verbose = F)$centroids)
-      },
-      "louvain_seurat" = {
-        Seurat::FindClusters(Seurat::FindNeighbors(X, annoy.metric = "cosine")$snn)
-      },
-      "flowClust" = {
-        flowClust::flowClust(X, K = 10)
-      }
-      #"MUDAN_igraph::cluster_walktrap" = {MUDAN::getComMembership(X, k = 30, method = igraph::cluster_walktrap)},
-    )
-  }
-)
-
-
-# cluster_walktrap failed at 50k:
-# Error in graph.adjacency.dense(adjmatrix, mode = mode, weighted = weighted,  : long vectors not supported yet: /Volumes/Builds/R4/R-4.1.0/src/include/Rinlinedfuns.h:537
-# cluster louvain failed at 50k cells (memory exhausted)
-# hclust failed at 100k cells
-
-clust_bench$method = attr(clust_bench$expression, "description")
-d <- as_tibble(clust_bench) %>% select(-expression, -result, -memory, -time, -gc)
-
-# https://github.com/slowkow/slowkow.com/blob/master/content/notes/pca-benchmark/index.Rmd
-p1 <- ggplot(d, aes(n, median, color = method, label = reorder(method, n / median))) +
-  geomtextpath::geom_textline(aes(label = method), size = 5.5, text_smoothing = 30) +
-  guides(color = guide_legend(reverse = FALSE,
-                              override.aes = list(label = levels(fct_reorder(d$method, d$n / d$median)), size = 5),
-                              label = FALSE)) +
-  ggrepel::geom_text_repel(data = d %>% filter(n == max(n)),
-                           mapping = aes(label = glue::glue("{signif(median, 3)}s")),
-                           size = 6,
-                           nudge_x = 0.1,
-                           box.padding = 0.1,
-                           direction = "y", hjust = 0,
-                           segment.size = NA) +
-  scale_x_log10(labels = scales::label_number_si(), expand = expansion(mult = c(0, 0.3))) +
-  scale_y_log10(labels = function(x) (x), name = "Seconds", expand = expansion(mult = c(0, 0.2), add = 0)) +
-  theme(legend.position = "none",panel.grid.major = element_line(size = 0.3, color = "grey90")) +
-  annotation_logticks(side = "lb") +
-  labs(title = "Seconds elapsed", x = "Cells")
-
-p2 <- ggplot(d, aes(n, mem_alloc, color = method, label = reorder(method, -n / median))) +
-  geomtextpath::geom_textline(size = 5.5, text_smoothing = 30) +
-  guides(color = guide_legend(reverse = TRUE, override.aes = list(label = "", size = 3))) +
-  ggrepel::geom_text_repel(data = d %>% filter(n == max(n)),
-                           mapping = aes(label = as.character(mem_alloc)),
-                           size = 6,
-                           nudge_x = 0.1,
-                           box.padding = 0.1,
-                           hjust = 0,
-                           direction = "y") +
-  scale_x_log10(labels = scales::label_number_si(), expand = expansion(mult = c(0, 0.3))) +
-  scale_y_log10(labels = function(x) (x / 1e9), name = "GB", expand = expansion(mult = c(0, 0.2), add = 0)) +
-  theme(legend.position = "none", panel.grid.major = element_line(size = 0.3, color = "grey90")) +
-  annotation_logticks(side = "lb") +
-  labs(title = "GB allocated", x = "Cells")
-
-p_grid <- cowplot::plot_grid(p1, p2)
-ggsave(p_grid, filename = paste0("first_round_50k_to_200k_cells.png"), device = "png", path = wd, dpi = "retina", width = 18, height = 8)
 
