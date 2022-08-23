2d2ef6444366113a246250952045358954d57749
Christopher Skopnik <christopher.skopnik@MAC20220426.drfz.de>
2022-08-23 23:39:53 +0200

    up

--- R/wsx_compMats_to_fcs.R
+++ R/wsx_compMats_to_fcs.R
@@ -8,6 +8,8 @@
 #' @param ws path to flowjo workspace
 #' @param ... additional arguments to fcexpr:::prep_spill()
 #' @param groups which flowjo groups to get fcs files from
+#' @param alt_FCS_file_folder if FCS files paths on disk are not the same as
+#' in flowjos wsp file, then provide the correct path here
 #'
 #' @return no return but SPILL keyword updated in FCS files
 #' @export
@@ -18,6 +20,7 @@
 #'}
 wsx_compMats_to_fcs <- function(ws,
                                 groups = NULL,
+                                alt_FCS_file_folder = NULL,
                                 ...) {
 
   if (!requireNamespace("CytoML", quietly = T)){
@@ -54,6 +57,55 @@ wsx_compMats_to_fcs <- function(ws,
     gsub("^file:", "", xml2::xml_attr(xml2::xml_child(ss[[n]], "DataSet"), "uri"))
   })
   compMats <- compMats[which(!sapply(compMats, is.null))]
+
+
+  if (!is.null(alt_FCS_file_folder)) {
+    # replace path in compMats list with path to alt_FCS_file
+    # match files via identities (the custom, own one)
+    # get identity from keyword entries in wsp file below
+    # make this a seperate functions somewhen
+
+    k <- wsx_get_keywords(ws = ws)
+    kk <- do.call(rbind, k)
+    kk$FileName <- rep(names(k), sapply(k,nrow))
+    kk <- dplyr::distinct(kk)
+
+    ## taken from .check.FCS.files - made analogous
+    filenames <- unique(kk$FileName)
+    dd <- kk[which(kk$name == "$DATE"), "value"]
+    tt <- kk[which(kk$name == "$BTIM"), "value"]
+    et <- kk[which(kk$name == "$ETIM"), "value"]
+
+    fil <- kk[which(kk$name == "$FIL"), "value"]
+    tot <- kk[which(kk$name == "$TOT"), "value"]
+
+    if (any(nchar(tt) - nchar(gsub(":", "", tt)) > 2)) {
+      tt_fix_ind <- which(nchar(tt) - nchar(gsub(":", "", tt)) > 2)
+      tt[tt_fix_ind] <- paste(rev(rev(strsplit(tt[tt_fix_ind], ":")[[1]])[-1]), collapse = ":")
+    }
+    datetime <- paste0(dd, "-", tt)
+    sub <- ifelse(grepl("^2[[:digit:]]", tt) & grepl("^0[[:digit:]]", et), 86400, 0)
+    datetime <- format(lubridate::parse_date_time(datetime, orders = c("%Y-%b-%d-%H:%M:%S", "%Y-%B-%d-%H:%M:%S", "%Y-%m-%d-%H:%M:%S", "%d-%b-%Y-%H:%M:%S",
+                                                                       "%d-%m-%Y-%H:%M:%S", "%d-%B-%Y-%H:%M:%S", "%d-%b-%Y-%H:%M:%S")) - sub, "%Y.%m.%d-%H.%M.%S")
+    if (any(is.na(datetime))) {
+      warning("datetimes ", paste(paste0(dd, "-", tt)[which(is.na(datetime))], collapse = ", "), " could not be converted to a uniform format. Please, provide this to the package-maintainer.")
+    }
+    fcs.files <- stats::setNames(paste0(fil, "_-_", trimws(tot), "_-_", datetime), nm = filenames)
+    if (length(unique(fcs.files)) != length(fcs.files)) {
+      stop(paste0("Duplicate FCS files found. This is not allowed. Please, remove one of each duplicates. \n", paste(names(fcs.files[duplicated(fcs.files) |
+                                                                                                                                       duplicated(fcs.files, fromLast = T)]), collapse = "\n")))
+    }
+
+    alt_fcs_files <- .check.FCS.files(alt_FCS_file_folder)
+    # replace paths with identity
+    names(compMats) <- unname(fcs.files[basename(names(compMats))])
+    alt_fcs_files <- alt_fcs_files[which(alt_fcs_files %in% names(compMats))] # actually not needed
+    compMats <- compMats[which(names(compMats) %in% alt_fcs_files)]
+    alt_fcs_files_rev <- stats::setNames(names(alt_fcs_files), nm = alt_fcs_files)
+    names(compMats) <- alt_fcs_files_rev[names(compMats)]
+    message(length(names(compMats)), " FCS files in alt_FCS_file_folder matched with those from the workspace. CompMats will be written to those.")
+  }
+
 
   for (i in seq_along(compMats)) {
     if (!file.exists(names(compMats)[i])) {
