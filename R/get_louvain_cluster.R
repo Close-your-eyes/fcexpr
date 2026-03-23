8f67f74e8dd2907aef8b411e89b951f8d59cfe80
Close-your-eyes <vonskopnik@pm.me>
2026-03-23 10:59:47 +0100

    up

--- R/get_louvain_cluster.R
+++ R/get_louvain_cluster.R
@@ -4,6 +4,11 @@
 #' @param FindNeighbors_args arguments to Seurat::FindNeighbors
 #' @param FindClusters_args arguments to Seurat::FindClusters
 #' @param mc.cores cores to use to calculate multiple resolutions in parallel
+#' @param return_as
+#' @param pad_len
+#' @param unique_only
+#' @param one_res_per_cluster_num_only
+#' @param min_cluster
 #'
 #' @return matrix with cluster identities
 #' @export
@@ -15,9 +20,20 @@
 get_louvain_cluster <- function(exprs,
                                 FindNeighbors_args = list(),
                                 FindClusters_args = list(resolution = c(0.1), verbose = T),
-                                mc.cores = 1) {
+                                mc.cores = 1,
+                                return_as = c("character", "numeric"),
+                                pad_len = "overall_max",
+                                unique_only = T,
+                                one_res_per_cluster_num_only = F,
+                                min_cluster = 1) {
+
+  return_as <- rlang::arg_match(return_as)
+
   mc.cores <- min(mc.cores, parallel::detectCores()-1)
-  rownames(exprs) <- 1:nrow(exprs)
+  if (is.null(rownames(exprs))) {
+    rownames(exprs) <- 1:nrow(exprs)
+  }
+
   snn <- suppressMessages(suppressWarnings(do.call(Seurat::FindNeighbors, args = c(list(object = exprs), FindNeighbors_args))))
 
   if (any(grepl("resolution", names(FindClusters_args), ignore.case = T)) && length(FindClusters_args[["resolution"]]) > 1) {
@@ -28,5 +44,34 @@ get_louvain_cluster <- function(exprs,
   } else {
     clust_idents <- apply(do.call(Seurat::FindClusters, args = c(list(object = snn$snn), FindClusters_args)), 2, as.numeric)
   }
+
+  if (return_as == "character") {
+    if (pad_len == "overall_max") {
+      pad_len <- nchar(max(clust_idents))
+    }
+    clust_idents <- apply(clust_idents, 2, as.character)
+    clust_idents <- apply(clust_idents, 2, function(x) brathering::pad_num_in_str(x = x, len = pad_len))
+  }
+
+  if (unique_only) {
+    hashes <- apply(clust_idents, 2, digest::digest)
+    cl_unique <- hashes[!duplicated(hashes)]
+    clust_idents <- clust_idents[,names(cl_unique), drop = F]
+  }
+
+  if (one_res_per_cluster_num_only) {
+    nn <- apply(clust_idents, 2, function(x) length(unique(x)))
+    clust_idents <- clust_idents[,names(nn[!duplicated(nn)])]
+  }
+
+  clust_idents <- clust_idents[,which(apply(clust_idents, 2, function(x) length(unique(x))) >= min_cluster)]
+  if (ncol(clust_idents) == 0) {
+    message("no clusterings left. decrease min_cluster?")
+  } else {
+    rownames(clust_idents) <- rownames(exprs)
+  }
+
   return(clust_idents)
 }
+
+
